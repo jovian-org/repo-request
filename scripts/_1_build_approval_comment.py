87e89c070b77ee9793d40de81ca35f017ce8ff91
jovianheok <jovianheok@gmail.com>
2026-06-16 10:19:23 +0800

    build: display issue form in comments for approval

---
+++ scripts/_1_build_approval_comment.py
@@ -0,0 +1,30 @@
+"""
+Purpose: Build a human-friendly display of the issue form for the approval to read in email notification
+"""
+
+import yaml
+import sys
+
+with open(sys.argv[1]) as f:
+    request = yaml.safe_load(f)
+
+print(
+f"""## Repository Request
+
+**Repository Name**
+{request.get('repo_name', '')}
+
+**Purpose**
+{request.get('repo_purpose', '')}
+
+**Owner Team**
+{request.get('owner_team', '')}
+
+**Additional Notes**
+{request.get('additional_notes') or 'None'}
+
+Reply with:
+- /approve
+- /reject
+"""
+)
